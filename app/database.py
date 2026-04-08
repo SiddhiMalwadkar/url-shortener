b926c3011eec643ac54a5b469adc5292319e79ca
Siddhi <malwadkarsiddhigirish@gmail.com>
2026-04-08 12:18:47 +0530

    Updated

--- app/database.py
+++ app/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Girish%40197703@localhost/url_db"
+import os
+
+DATABASE_URL = os.getenv("postgresql://postgres:VIRbjxwrlMebbsEtRrLlXGfoSjPIQsZc@postgres.railway.internal:5432/railway")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
